05fccb5d16154fb4511d93c86b5abf39e58868fc
ajmalhassan <knajmalhassan@gmail.com>
2017-11-07 15:31:15 +0530

    new db added

--- phr.sql
+++ phr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2017 at 04:53 AM
+-- Generation Time: Nov 07, 2017 at 11:00 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -49,15 +49,10 @@ CREATE TABLE `doctor_record` (
 CREATE TABLE `patient_health_record` (
   `r_id` int(255) NOT NULL,
   `p_id` int(255) NOT NULL,
-  `blood_group` varchar(200) NOT NULL,
-  `height` int(255) NOT NULL,
-  `weight` int(255) NOT NULL,
-  `allergies` varchar(1000) NOT NULL,
-  `insurance` varchar(1000) NOT NULL,
-  `blood_test_report` varchar(10000) NOT NULL,
   `medical_report` varchar(10000) NOT NULL,
   `last_consulted_dr` varchar(1000) NOT NULL,
-  `last_consulted_dr_phone` int(255) NOT NULL
+  `last_consulted_dr_phone` int(255) NOT NULL,
+  `consulted_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -70,11 +65,15 @@ CREATE TABLE `patient_record` (
   `r_id` int(255) NOT NULL,
   `p_id` int(255) NOT NULL,
   `first_name` varchar(200) NOT NULL,
-  `middle_name` varchar(200) NOT NULL,
+  `middle_name` varchar(200) DEFAULT NULL,
   `last_name` varchar(200) NOT NULL,
   `gender` varchar(100) NOT NULL,
-  `dob` date NOT NULL,
-  `age` int(10) NOT NULL,
+  `dob` varchar(100) NOT NULL,
+  `blood_group` varchar(100) NOT NULL,
+  `height` int(255) NOT NULL,
+  `weight` int(255) NOT NULL,
+  `allergy` varchar(400) NOT NULL,
+  `insurance` varchar(50) NOT NULL,
   `address` varchar(500) NOT NULL,
   `city` varchar(200) NOT NULL,
   `state` varchar(200) NOT NULL,
@@ -85,6 +84,13 @@ CREATE TABLE `patient_record` (
   `email` varchar(200) NOT NULL,
   `emergency-contact` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient_record`
+--
+
+INSERT INTO `patient_record` (`r_id`, `p_id`, `first_name`, `middle_name`, `last_name`, `gender`, `dob`, `blood_group`, `height`, `weight`, `allergy`, `insurance`, `address`, `city`, `state`, `nationality`, `pincode`, `marital_status`, `phone`, `email`, `emergency-contact`) VALUES
+(1, 21, 'demo', 'demo', 'demo', 'Male', '2017-11-11', 'O+', 123, 123, 'no', 'yes', 'Runa Manzil JRA 65,, Janatha Stop, Manjummel P.O,', 'Port Blair*', 'Andaman and Nicobar Islands', 'India', 683501, 'Single', 2147483647, 'demo@demo.demo', 21312);
 
 -- --------------------------------------------------------
 
@@ -104,7 +110,9 @@ CREATE TABLE `reg_users` (
 --
 
 INSERT INTO `reg_users` (`id`, `username`, `password`, `desig`) VALUES
-(2, 'Rasmiya', '1234', 'Dr.');
+(3, 'ajmalhassan', '1234', 'Doctor'),
+(4, 'Rasmiya', '1234', 'Patient'),
+(21, 'demo', 'demo', 'Patient');
 
 --
 -- Indexes for dumped tables
@@ -135,7 +143,8 @@ ALTER TABLE `patient_record`
 -- Indexes for table `reg_users`
 --
 ALTER TABLE `reg_users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -145,7 +154,7 @@ ALTER TABLE `reg_users`
 -- AUTO_INCREMENT for table `doctor_record`
 --
 ALTER TABLE `doctor_record`
-  MODIFY `r_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `r_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `patient_health_record`
 --
@@ -155,12 +164,12 @@ ALTER TABLE `patient_health_record`
 -- AUTO_INCREMENT for table `patient_record`
 --
 ALTER TABLE `patient_record`
-  MODIFY `r_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `r_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `reg_users`
 --
 ALTER TABLE `reg_users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- Constraints for dumped tables
 --
